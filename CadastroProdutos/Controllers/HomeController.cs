8d5adb765378bcd6ed0dced6de0574602a1146f6
Galvas <galvaomercante@gmail.com>
2025-07-31 09:13:43 -0300

    Ignorando arquivos temporários do VS

---
+++ CadastroProdutos/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+using System.Diagnostics;
+using CadastroProdutos.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroProdutos.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
